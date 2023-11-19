9652a923bf1960e105961205d247d9cda6876263
Mary Lens <marycravts@gmail.com>
2023-11-19 22:21:12 +0200

    Three maps added

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -5,7 +5,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -26,10 +27,260 @@ public class HelloApplication extends Application {
     private int endY = -1;
 
     //map initialisation
-    private final int[][] map = new int[WIDTH][HEIGHT];
-    private final boolean[][] path = new boolean[WIDTH][HEIGHT];
+    private int[][] map = new int[WIDTH][HEIGHT];
+    private boolean[][] path = new boolean[WIDTH][HEIGHT];
     boolean[][] visited = new boolean[WIDTH][HEIGHT];
     List<Integer[]> pathList = new ArrayList<>();
+
+    private void drawMap(GraphicsContext gc) {
+        gc.clearRect(0, 0, WIDTH * 8, HEIGHT * 8);
+
+        int cellSize = 8;
+
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (x == startX && y == startY) {
+                    gc.setFill(javafx.scene.paint.Color.BLUE);
+                } else if (x == endX && y == endY) {
+                    gc.setFill(Color.RED);
+                } else if (map[x][y] == 0) {
+                    gc.setFill(Color.BLACK);
+                } else if (path[x][y]) {
+                    gc.setFill(Color.CHARTREUSE);
+                } else if (visited[x][y]) {
+                    gc.setFill(Color.YELLOW);
+                } else {
+                    gc.setFill(javafx.scene.paint.Color.WHITE);
+                }
+                gc.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+            }
+        }
+    }
+
+    //start method for javaFX
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Map Visualization");
+        mapChis();
+
+        Canvas centralCanvas = createCanvas();
+        Canvas leftCanvas = createCanvas();
+        Canvas rightCanvas = createCanvas();
+//        Canvas canvas = new Canvas(WIDTH * 10, HEIGHT * 10);
+//        GraphicsContext gc = canvas.getGraphicsContext2D();
+//        drawMap(gc);
+
+        Button resetButton = new Button("Reset");
+        resetButton.setOnAction(event -> {
+            startX = -1;
+            startY = -1;
+            endX = -1;
+            endY = -1;
+            path = new boolean[WIDTH][HEIGHT];
+            visited = new boolean[WIDTH][HEIGHT];
+            pathList = new ArrayList<>();
+            drawMap(centralCanvas.getGraphicsContext2D());
+            drawMap(leftCanvas.getGraphicsContext2D());
+            drawMap(rightCanvas.getGraphicsContext2D());
+        });
+
+//        Pane root = new Pane();
+//        root.getChildren().add(canvas);
+        HBox root = new HBox(8);
+        root.getChildren().addAll(leftCanvas, centralCanvas, rightCanvas);
+        root.getChildren().add(resetButton);
+
+        centralCanvas.setOnMouseClicked(event -> handleCanvasClick(centralCanvas, event, leftCanvas, centralCanvas, rightCanvas));
+        leftCanvas.setOnMouseClicked(event -> handleCanvasClick(leftCanvas, event, leftCanvas, centralCanvas, rightCanvas));
+        rightCanvas.setOnMouseClicked(event -> handleCanvasClick(rightCanvas, event, leftCanvas, centralCanvas, rightCanvas));
+
+//        canvas.setOnMouseClicked(event -> {
+//            int x = (int) (event.getX() / 10);
+//            int y = (int) (event.getY() / 10);
+//
+//            if (startX == -1) {
+//                if (map[x][y] != 0) {
+//                    startX = x;
+//                    startY = y;
+//                    drawMap(gc);
+//                }
+//            } else if (endX == -1) {
+//                if (map[x][y] != 0) {
+//                    endX = x;
+//                    endY = y;
+//                    drawMap(gc);
+//                    int min_dist = findShortestPathLi(map, startX, startY, endX, endY);
+//                    drawMap(gc);
+//                    if (min_dist != -1) {
+//                        int stepDelayMillis = 100;
+//                        displayPathStepByStep(gc, stepDelayMillis);
+//                        System.out.println("The shortest path from source to destination has length " + min_dist);
+//                    } else {
+//                        System.out.println("Destination cannot be reached from source");
+//                    }
+//                }
+//            }
+//        });
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    private Canvas createCanvas() {
+        Canvas canvas = new Canvas(WIDTH * 8, HEIGHT * 8);
+        drawMap(canvas.getGraphicsContext2D());
+        return canvas;
+    }
+
+    private void handleCanvasClick(Canvas canvas, javafx.scene.input.MouseEvent event, Canvas leftCanvas, Canvas centralCanvas, Canvas rightCanvas) {
+        int x = (int) (event.getX() / 8);
+        int y = (int) (event.getY() / 8);
+
+        if (startX == -1) {
+            if (map[x][y] != 0) {
+                startX = x;
+                startY = y;
+                drawMap(leftCanvas.getGraphicsContext2D());
+                drawMap(rightCanvas.getGraphicsContext2D());
+                drawMap(centralCanvas.getGraphicsContext2D());
+            }
+        } else if (endX == -1) {
+            if (map[x][y] != 0) {
+                endX = x;
+                endY = y;
+                drawMap(leftCanvas.getGraphicsContext2D());
+                drawMap(rightCanvas.getGraphicsContext2D());
+                drawMap(centralCanvas.getGraphicsContext2D());
+                int min_dist;
+
+                if (canvas.equals(leftCanvas)) {
+                    min_dist = findShortestPathLi(map, startX, startY, endX, endY);
+                } else if (canvas.equals(rightCanvas)) {
+                    min_dist = findShortestPathLi(map, startX, startY, endX, endY);
+                } else {
+                    min_dist = findShortestPathLi(map, startX, startY, endX, endY);
+                }
+
+                drawMap(canvas.getGraphicsContext2D());
+
+                if (min_dist != -1) {
+                    int stepDelayMillis = 100;
+                    displayPathStepByStep(canvas.getGraphicsContext2D(), stepDelayMillis);
+                    System.out.println("The shortest path from source to destination has length " + min_dist);
+                } else {
+                    System.out.println("Destination cannot be reached from source");
+                }
+            }
+        }
+    }
+
+    //to display path beautifully, algorithm Li
+    private void displayPathStepByStep(GraphicsContext gc, int stepDelayMillis) {
+        if (pathList.size() > 0) {
+            Integer[] pathCoords = pathList.remove(pathList.size() - 1);
+            path[pathCoords[0]][pathCoords[1]] = true;
+            drawMap(gc);
+
+            PauseTransition pause = new PauseTransition(Duration.millis(stepDelayMillis));
+            pause.setOnFinished(event -> displayPathStepByStep(gc, stepDelayMillis));
+            pause.play();
+        }
+    }
+
+    //possible movements, 8 ways, coordinates according to the combination of pairs (row[i],col[i])
+    private static final int[] row = {-1, -1, 0, 1, 1, 1, 0, -1};
+    private static final int[] col = {0, -1, -1, -1, 0, 1, 1, 1};
+
+    //check if chosen move is valid
+    private static boolean isValid(int[][] mat, boolean[][] visited, int x, int y) {
+        return (x >= 0) && (x < mat.length) && (y >= 0) && (y < mat[0].length)
+                && mat[x][y] == 1 && !visited[x][y];
+    }
+
+    //overloading of isValid method considering the direction of the road
+    private static boolean isValid(int[][] mat, boolean[][] visited, int x, int y, int directionX, int directionY) {
+        if ((x < 0) || (x >= mat.length) || (y < 0) || (y >= mat[0].length) || mat[x][y] == 0 || visited[x][y]) {
+            return false;
+        }
+        //check if cell has only one direction
+        //left
+        else if (!(directionX == -1 && directionY == 0) && mat[x - directionX][y - directionY] == 4) {
+            return false;
+        } //left-up
+        else if (!(directionX == -1 && directionY == -1) && mat[x - directionX][y - directionY] == 6) {
+            return false;
+        } //up
+        else if (!(directionX == 0 && directionY == -1) && mat[x - directionX][y - directionY] == 2) {
+            return false;
+        } //right-up
+        else if (!(directionX == 1 && directionY == -1) && mat[x - directionX][y - directionY] == 7) {
+            return false;
+        } //right
+        else if (!(directionX == 1 && directionY == 0) && mat[x - directionX][y - directionY] == 5) {
+            return false;
+        } //right-down
+        else if (!(directionX == 1 && directionY == 1) && mat[x - directionX][y - directionY] == 9) {
+            return false;
+        } //down
+        else if (!(directionX == 0 && directionY == 1) && mat[x - directionX][y - directionY] == 3) {
+            return false;
+        } //left-down
+        else if (!(directionX == -1 && directionY == 1) && mat[x - directionX][y - directionY] == 8) {
+            return false;
+        }
+        return true;
+    }
+
+    //algorithm Li
+    private int findShortestPathLi(int[][] mat, int i, int j, int x, int y) {
+        if (mat == null || mat.length == 0 || mat[i][j] == 0 || mat[x][y] == 0) {
+            return -1;
+        }
+
+        int M = mat.length;
+        int N = mat[0].length;
+
+        Queue<Node> q = new ArrayDeque<>();
+
+        visited[i][j] = true;
+        q.add(new Node(i, j, 0, null));
+
+        int min_dist = Integer.MAX_VALUE;
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+
+            i = node.x;
+            j = node.y;
+            int dist = node.dist;
+
+            if (i == x && j == y) {
+                min_dist = dist;
+                while (node != null) {
+                    pathList.add(new Integer[]{node.x, node.y});
+                    node = node.prev;
+                }
+                break;
+            }
+
+            for (int k = 0; k < 8; k++) {
+                if (isValid(mat, visited, i + row[k], j + col[k], row[k], col[k])) {
+                    visited[i + row[k]][j + col[k]] = true;
+                    q.add(new Node(i + row[k], j + col[k], dist + 1, node));
+                }
+            }
+        }
+
+        if (min_dist != Integer.MAX_VALUE) {
+            return min_dist;
+        }
+        return -1;
+    }
+
+    //main method
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     // map generated randomly
     private void map() {
@@ -43,6 +294,7 @@ public class HelloApplication extends Application {
             }
         }
     }
+
     // map generated using Chisinau pototype
     private void mapChis() {
         for (int i = 0; i < map.length; i++) {
@@ -668,7 +920,7 @@ public class HelloApplication extends Application {
         map[32][50] = 1;
         map[33][50] = 5;
         map[34][50] = 5;
-        map[35][50] =1;
+        map[35][50] = 1;
         map[36][50] = 5;
         map[37][50] = 5;
         map[38][50] = 5;
@@ -838,183 +1090,5 @@ public class HelloApplication extends Application {
 
         map[28][69] = 1;
         map[43][69] = 1;
-    }
-
-    private void drawMap(GraphicsContext gc) {
-        gc.clearRect(0, 0, WIDTH * 10, HEIGHT * 10);
-
-        int cellSize = 10;
-
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                if (x == startX && y == startY) {
-                    gc.setFill(javafx.scene.paint.Color.BLUE);
-                } else if (x == endX && y == endY) {
-                    gc.setFill(Color.RED);
-                } else if (map[x][y] == 0) {
-                    gc.setFill(Color.BLACK);
-                } else if (path[x][y]) {
-                    gc.setFill(Color.CHARTREUSE);
-                } else if (visited[x][y]) {
-                    gc.setFill(Color.YELLOW);
-                } else {
-                    gc.setFill(javafx.scene.paint.Color.WHITE);
-                }
-                gc.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-            }
-        }
-    }
-
-    //start method for javaFX
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Map Visualization");
-        mapChis();
-
-        Canvas canvas = new Canvas(WIDTH * 10, HEIGHT * 10);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        drawMap(gc);
-
-        Pane root = new Pane();
-        root.getChildren().add(canvas);
-
-        canvas.setOnMouseClicked(event -> {
-            int x = (int) (event.getX() / 10);
-            int y = (int) (event.getY() / 10);
-
-            if (startX == -1) {
-                if (map[x][y] != 0) {
-                    startX = x;
-                    startY = y;
-                    drawMap(gc);
-                }
-            } else if (endX == -1) {
-                if (map[x][y] != 0) {
-                    endX = x;
-                    endY = y;
-                    drawMap(gc);
-                    int min_dist = findShortestPathLi(map, startX, startY, endX, endY);
-                    drawMap(gc);
-                    if (min_dist != -1) {
-                        int stepDelayMillis = 100;
-                        displayPathStepByStep(gc, stepDelayMillis);
-                        System.out.println("The shortest path from source to destination has length " + min_dist);
-                    } else {
-                        System.out.println("Destination cannot be reached from source");
-                    }
-                }
-            }
-        });
-
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
-
-    //to display path beautifully, algorithm Li
-    private void displayPathStepByStep(GraphicsContext gc, int stepDelayMillis) {
-        if (pathList.size() > 0) {
-            Integer[] pathCoords = pathList.remove(pathList.size() - 1);
-            path[pathCoords[0]][pathCoords[1]] = true;
-            drawMap(gc);
-
-            PauseTransition pause = new PauseTransition(Duration.millis(stepDelayMillis));
-            pause.setOnFinished(event -> displayPathStepByStep(gc, stepDelayMillis));
-            pause.play();
-        }
-    }
-
-    //possible movements, 8 ways, coordinates according to the combination of pairs (row[i],col[i])
-    private static final int[] row = {-1, -1, 0, 1, 1, 1, 0, -1};
-    private static final int[] col = {0, -1, -1, -1, 0, 1, 1, 1};
-
-    //check if chosen move is valid
-    private static boolean isValid(int[][] mat, boolean[][] visited, int x, int y) {
-        return (x >= 0) && (x < mat.length) && (y >= 0) && (y < mat[0].length)
-                && mat[x][y] == 1 && !visited[x][y];
-    }
-
-    //overloading of isValid method considering the direction of the road
-    private static boolean isValid(int[][] mat, boolean[][] visited, int x, int y, int directionX, int directionY) {
-        if ((x < 0) || (x >= mat.length) || (y < 0) || (y >= mat[0].length) || mat[x][y] == 0 || visited[x][y]) {
-            return false;
-        }
-        //check if cell has only one direction
-        //left
-        else if (!(directionX == -1 && directionY == 0) && mat[x-directionX][y-directionY] == 4) {
-            return false;
-        } //left-up
-        else if (!(directionX == -1 && directionY == -1) && mat[x-directionX][y-directionY] == 6) {
-            return false;
-        } //up
-        else if (!(directionX == 0 && directionY == -1) && mat[x-directionX][y-directionY] == 2) {
-            return false;
-        } //right-up
-        else if (!(directionX == 1 && directionY == -1) && mat[x-directionX][y-directionY] == 7) {
-            return false;
-        } //right
-        else if (!(directionX == 1 && directionY == 0) && mat[x-directionX][y-directionY] == 5) {
-            return false;
-        } //right-down
-        else if (!(directionX == 1 && directionY == 1) && mat[x-directionX][y-directionY] == 9) {
-            return false;
-        } //down
-        else if (!(directionX == 0 && directionY == 1) && mat[x-directionX][y-directionY] == 3) {
-            return false;
-        } //left-down
-        else if (!(directionX == -1 && directionY == 1) && mat[x-directionX][y-directionY] == 8) {
-            return false;
-        }
-        return true;
-    }
-
-    //algorithm Li
-    private int findShortestPathLi(int[][] mat, int i, int j, int x, int y) {
-        if (mat == null || mat.length == 0 || mat[i][j] == 0 || mat[x][y] == 0) {
-            return -1;
-        }
-
-        int M = mat.length;
-        int N = mat[0].length;
-
-        Queue<Node> q = new ArrayDeque<>();
-
-        visited[i][j] = true;
-        q.add(new Node(i, j, 0, null));
-
-        int min_dist = Integer.MAX_VALUE;
-
-        while (!q.isEmpty()) {
-            Node node = q.poll();
-
-            i = node.x;
-            j = node.y;
-            int dist = node.dist;
-
-            if (i == x && j == y) {
-                min_dist = dist;
-                while (node != null) {
-                    pathList.add(new Integer[]{node.x, node.y});
-                    node = node.prev;
-                }
-                break;
-            }
-
-            for (int k = 0; k < 8; k++) {
-                if (isValid(mat, visited, i + row[k], j + col[k], row[k], col[k])) {
-                    visited[i + row[k]][j + col[k]] = true;
-                    q.add(new Node(i + row[k], j + col[k], dist + 1, node));
-                }
-            }
-        }
-
-        if (min_dist != Integer.MAX_VALUE) {
-            return min_dist;
-        }
-        return -1;
-    }
-
-    //main method
-    public static void main(String[] args) {
-        launch(args);
     }
 }
